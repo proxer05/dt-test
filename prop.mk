80db0034132d9d1255520061662ecb8d5e6d988f
Sujin Panicker <spanic@codeaurora.org>
2019-07-18 14:39:28 +0530

    daisy: prop: Fix rank of OMX audio codec components
    Rank OMX SW audio codecs higher than C2 codecs.
    
    Change-Id: I7d03179ebd313d2592512178eeef266b4243f2f0
    (cherry picked from commit 57f6ea3d4b6db71d1551ac0c8d31458c78d321d3)

--- prop.mk
+++ prop.mk
@@ -172,7 +172,9 @@ vendor.vidc.enc.disable_bframes=1 \
 vendor.video.disable.ubwc=1 \
 vendor.display.enable_default_color_mode=1 \
 vendor.gralloc.enable_fb_ubwc=1 \
-vendor.video.disable.ubwc=1
+vendor.video.disable.ubwc=1 \
+debug.stagefright.omx_default_rank.sw-audio=1 \
+debug.stagefright.omx_default_rank=0
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
