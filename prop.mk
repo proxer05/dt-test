12a82c2635c6250fc93901ecad81120dd95c7175
Baldev Sahu <bsahu@codeaurora.org>
2020-10-29 14:26:09 +0100

    daisy: Disable ahardware buffer
    - Disable ahardware buffer usage in gralloc
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- prop.mk
+++ prop.mk
@@ -131,7 +131,8 @@ vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
 vendor.gralloc.enable_fb_ubwc=1 \
 debug.hwui.renderer=skiagl \
-video.accelerate.hw=1
+video.accelerate.hw=1 \
+vendor.gralloc.disable_ahardware_buffer=1
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
