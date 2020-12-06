3ba77a7bb023e9bd7b4ec1591bf363c8aa6fece0
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-12-06 14:58:04 +0100

    daisy: Upgrade BT stack from sunfish
    - Also disable A2DP offload
    
    * From: sunfish-user 11 RP1A.200720.011 6746289 release-keys

--- prop.mk
+++ prop.mk
@@ -36,10 +36,13 @@ ro.vendor.audio.sdk.fluencetype=fluence
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-bluetooth.hfp.client=1 \
-qcom.bluetooth.soc=smd \
-ro.bluetooth.hfp.ver=1.7 \
-ro.qualcomm.bt.hci_transport=smd
+persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
+persist.bluetooth.a2dp_offload.disabled=false \
+persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive \
+persist.vendor.qcom.bluetooth.enable.splita2dp=false \
+persist.vendor.qcom.bluetooth.soc=pronto \
+ro.bluetooth.a2dp_offload.supported=false \
+vendor.qcom.bluetooth.soc=pronto
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
