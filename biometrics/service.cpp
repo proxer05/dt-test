f33c43891bd05053bfd51c220aebf12202a077b5
Hotorou <zunayeddihan@gmail.com>
2021-01-16 09:47:02 +0100

    daisy: biometrics: Rebrand for daisy
    Change-Id: I39f74f1bcf71e799516fa2e57a62c9a1c4ab4ca2
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- biometrics/service.cpp
+++ biometrics/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.daisy"
 
 #include <android/log.h>
 #include <hidl/HidlSupport.h>
