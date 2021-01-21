eb6179a6979459ce41971d7fa89ac2490789bd3f
SakilMondal <mondalsakil@gmail.com>
2021-01-21 12:54:05 +0530

    daisy: Drop non-functional soundtrigger
    Change-Id: I21100e2f368ec0ddf3b546372b241d85bb698747
    
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -99,7 +99,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
     audio.primary.msm8953:32 \
     audio.r_submix.default \
     audio.usb.default \
