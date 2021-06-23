7be2dbf7a35e7c6bfeadf412a8e05b6bb37b0317
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-06-23 19:41:17 +0300

    daisy: prop: Disable cam2api by default

--- prop.mk
+++ prop.mk
@@ -65,7 +65,6 @@ persist.vendor.camera.gyro.disable=0 \
 persist.vendor.camera.isp.clock.optmz=0 \
 persist.vendor.camera.stats.test=5 \
 persist.vendor.camera.CDS=off \
-persist.camera.HAL3.enabled=1 \
 persist.vendor.camera.llc=0
 
 # Charger
