8d0f390ab61ebf6e821f3467b13b1dd49b8ffff9
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-07-23 20:49:58 +0300

    daisy: Use Eva GCC to build the kernel inline
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,13 +46,11 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
-TARGET_KERNEL_CONFIG := sleepy_defconfig
+TARGET_KERNEL_CONFIG := sakura_defconfig
 TARGET_KERNEL_VERSION := 4.9
-TARGET_KERNEL_CLANG_COMPILE := true
-KERNEL_LD := LD=ld.lld
-TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-gnu-
-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-elf-
+KERNEL_TOOLCHAIN := $(PWD)/prebuilts/gcc/linux-x86/aarch64/aarch64-elf/bin
+KERNEL_LD := LD=$(PWD)/prebuilts/gcc/linux-x86/aarch64/aarch64-elf/bin/aarch64-elf-ld.lld
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
