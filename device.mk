cc5deb62e6b47047fada594a9f922280b364c005
ZeeLog <Danjel.Zammit@gmail.com>
2021-07-30 17:53:48 +0300

    daisy: Update Media stack from LA.UM.9.6.2.r1-03600-89xx.0
    Co-authored-by: ItsVixano <vixanonew@gmail.com>

--- device.mk
+++ device.mk
@@ -407,10 +407,6 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
     WifiOverlay
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
