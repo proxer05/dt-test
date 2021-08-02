e3a63aa9a75fb696b0b5067994a65b1d0739eac2
proxer05 <62423996+proxer05@users.noreply.github.com>
2021-08-02 23:31:46 +0200

    daisy: Update coral build fingerprint to August 2021

--- revengeos_daisy.mk
+++ revengeos_daisy.mk
@@ -46,8 +46,8 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:11/RQ3A.210705.001/7380771:user/release-keys
-BUILD_DESCRIPTION := coral-user 11 RQ3A.210705.001 7380771 release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_DESCRIPTION := coral-user 11 RQ3A.210805.001.A1 7474174 release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
