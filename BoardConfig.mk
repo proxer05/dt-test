beae69995869741062d2262bb7652d6173608b2b
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-09-01 00:17:08 +0300

    daisy: Update fakelogprint lib paths for source-built biometrics HAL
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,10 +192,10 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /vendor/lib64/hw/gf_fingerprint.default.so|fakelogprint.so \
+    /vendor/lib64/hw/gf_fingerprint.goodix.default.so|fakelogprint.so \
     /vendor/lib64/libgf_ca.so|fakelogprint.so \
     /vendor/lib64/libgf_hal.so|fakelogprint.so \
-    /vendor/lib64/hw/fingerprint.default.so|fakelogprint.so
+    /vendor/lib64/hw/fingerprint.fpc.default.so|fakelogprint.so
 
 # Telephony
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
